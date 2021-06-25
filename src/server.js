bf730ebb20793df84ed356f96f93ce54f7c8917c
tatialveso <tatiialveso@gmail.com>
2021-06-25 18:38:05 -0300

    :ok_hand: conect public folder

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const server = express();
 server.set('view engine', 'ejs');
 server.set('views', path.join(__dirname, 'views'));
+server.use(express.static("public"));
 server.use(route);
 
 server.listen(3000, () => console.log("Servidor rodando"));
