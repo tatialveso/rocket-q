f25cf28e3b4ca1dfee34aa381318f0f79f8a5e84
tatialveso <tatiialveso@gmail.com>
2021-06-26 19:54:48 -0300

    :sparkles: add modal changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,28 @@ import Modal from './modal.js';
 
 const modal = Modal();
 
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalBtn = document.querySelector(".modal button");
+
 // open modal when button clicked
 const checkBtn = document.querySelectorAll(".actions a.check");
 checkBtn.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open();
-    })
+    button.addEventListener("click", handleClick);
 })
 
 const deleteBtn = document.querySelectorAll(".actions a.delete");
 deleteBtn.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open();
-    })
+    button.addEventListener("click", (event) => handleClick(event, false));
 })
+
+function handleClick(event, check = true) {
+    modalTitle.innerHTML = check ? "Marcar como lida" : "Excluir esta pergunta";
+    modalDescription.innerHTML = check ? "Deseja marcar esta pergunta como lida?" : "Tem certeza que você deseja excluir esta pergunta?";
+    modalBtn.innerHTML = check ? "Sim, marcar como lida" : "Sim, excluir";
+
+    check ? modalBtn.classList.remove("red") : modalBtn.classList.add("red");
+
+    modal.open();
+
+}
